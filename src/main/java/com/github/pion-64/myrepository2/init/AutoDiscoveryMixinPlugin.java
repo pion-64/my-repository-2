7cdeb21f4dadbf0a65bd8fe4deca79fb2aafd362
GitHub Action <action@github.com>
2025-05-20 19:04:25 +0000

    Initialize template repository proper
    Set package name to com.github.pion-64.myrepository2
    Set project name to my-repository-2
    Set modid to myrepository

--- src/main/java/com/github/pion-64/myrepository2/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/pion-64/myrepository2/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.pion-64.myrepository2.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
